6c6259099d5124d4d41f3052cc6cc7535128cef2
SS-okamura <ss.okamura0509@gmail.com>
2024-05-15 13:00:55 +0900

    lark sdk config

---
+++ src/config/lark/config.ts
@@ -0,0 +1,8 @@
+import * as lark from '@larksuiteoapi/node-sdk';
+
+export const larkClient = new lark.Client({
+  appId: process.env.APP_ID,
+  appSecret: process.env.APP_SECRET,
+  appType: lark.AppType.SelfBuild,
+  domain: lark.Domain.Lark,
+});
