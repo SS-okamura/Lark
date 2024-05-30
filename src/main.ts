ad80b9acbbc9849c817ce0959c4469e388792293
SS-okamura <ss.okamura0509@gmail.com>
2024-05-30 18:13:45 +0900

    Swaggerの設定

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LarkToken } from './config/lark/lark.token';
+import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
+import { SampleMiddleware } from './common/middleware/sample/sample.middleware';
+import { HttpExceptionFilter } from './common/filters/http-exception-filter/http-exception-filter.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder().setTitle('Lark API テスト').build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
